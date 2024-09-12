e13869329dfbaedb24af350be37e19423c64edea
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-11 17:12:14 -0700

    Update tt_um_seven_segment_seconds.v

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -41,7 +41,7 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
                 digit <= digit + 1'b1;
 
                 // only count from 0 to 9
-                if (digit == 9)
+                if (digit == 8)
                     digit <= 0;
 
             end else
